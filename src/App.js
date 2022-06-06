39d60c172caef239b8075f68741ef15d9faf2bcd
Bart Gonzaga <102068508+bzaga18@users.noreply.github.com>
2022-06-06 14:35:14 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
